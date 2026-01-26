9cae3efc521ed789762e1d6147771c328556e1f5
Taiz2000 <zhoutaidi@outlook.com>
2026-01-27 01:13:16 +0800

    Add task functionality

--- src/main/java/Sagiri.java
+++ src/main/java/Sagiri.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Sagiri {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int index = 0;
 
         String name = "Sagiri";
         String bar = "____________________________________________________________";
@@ -13,11 +15,20 @@ public class Sagiri {
 
         String input = scanner.nextLine();
 
-        while (!input.equals("bye")) { // check if input is bye
-            // loop to read and echo user input
-            System.out.println(bar);
-            System.out.println(input);
-            System.out.println(bar);
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println(bar);
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i+1) + ". " + tasks[i]);
+                }
+                System.out.println(bar);
+            } else {
+                System.out.println(bar);
+                System.out.println("added: " + input);
+                tasks[index] = input; // add to array
+                index++;
+                System.out.println(bar);
+            }
             input = scanner.nextLine();
         }
 
