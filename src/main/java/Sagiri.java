845834f3bc66b82bc26a7b3508c7fc2e099b2771
Taiz2000 <zhoutaidi@outlook.com>
2026-01-26 23:18:13 +0800

    Add echo functionality

--- src/main/java/Sagiri.java
+++ src/main/java/Sagiri.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Sagiri {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String name = "Sagiri";
         String bar = "____________________________________________________________";
         System.out.println(bar);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(bar);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) { // check if input is bye
+            // loop to read and echo user input
+            System.out.println(bar);
+            System.out.println(input);
+            System.out.println(bar);
+            input = scanner.nextLine();
+        }
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(bar);
