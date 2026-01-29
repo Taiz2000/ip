8d37898af0ace6c7e0ee085278f309370b4b1064
Taiz2000 <zhoutaidi@outlook.com>
2026-01-29 15:54:19 +0800

    Add mark as done functionality

--- src/main/java/Sagiri.java
+++ src/main/java/Sagiri.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Sagiri {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        String[][] tasks = new String[100][2];
         int index = 0;
 
         String name = "Sagiri";
@@ -19,13 +19,29 @@ public class Sagiri {
             if (input.equals("list")) {
                 System.out.println(bar);
                 for (int i = 0; i < index; i++) {
-                    System.out.println((i+1) + ". " + tasks[i]);
+                    System.out.println((i+1) + ".[" + tasks[i][0] + "] " + tasks[i][1]);
+
                 }
+                System.out.println(bar);
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber][0] = "X";
+                System.out.println(bar);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks[taskNumber][0] + "] " + tasks[taskNumber][1]);
+                System.out.println(bar);
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber][0] = " ";
+                System.out.println(bar);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + tasks[taskNumber][0] + "] " + tasks[taskNumber][1]);
                 System.out.println(bar);
             } else {
                 System.out.println(bar);
                 System.out.println("added: " + input);
-                tasks[index] = input; // add to array
+                tasks[index][1] = input; // add to array
+                tasks[index][0] = " "; // default status
                 index++;
                 System.out.println(bar);
             }
@@ -34,5 +50,6 @@ public class Sagiri {
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(bar);
+        scanner.close();
     }
 }
