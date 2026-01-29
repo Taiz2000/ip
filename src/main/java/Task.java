a2c74f2453eba9cbc4370ee4ad3a090ce8324130
Taiz2000 <zhoutaidi@outlook.com>
2026-01-29 21:09:23 +0800

    Add support for task type as an enum

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,67 @@
 public class Task {
     private String name;
     private boolean isDone;
+    private TaskType type;
+    private String startDate;
+    private String endDate;
     
     /**
-     * Constructor for Task.
+     * Constructor for Task (ToDo).
      * @param name
      */
     public Task(String name) {
+        this(name, TaskType.TODO, null, null);
+    }
+
+    /**
+     * Constructor for Task (Event).
+     * @param name
+     * @param startDate
+     * @param endDate
+     */
+    public Task(String name, String startDate, String endDate) {
+        this(name, TaskType.EVENT, startDate, endDate);
+    }
+
+    /**
+     * Constructor for Task (Deadline).
+     * @param name
+     * @param endDate
+     */
+    public Task(String name, String endDate) {
+        this(name, TaskType.DEADLINE, null, endDate);
+    }
+
+    /**
+     * Private constructor for Task.
+     * @param name
+     * @param type
+     * @param startDate
+     * @param endDate
+     */
+    private Task(String name, TaskType type, String startDate, String endDate) {
         this.name = name;
+        this.type = type;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.isDone = false;
+    }
+
+    /**
+     * Gets the type icon.
+     * @return "T", "E", or "D"
+     */
+    public String getTypeIcon() {
+        switch (type) {
+        case TODO:
+            return "T";
+        case EVENT:
+            return "E";
+        case DEADLINE:
+            return "D";
+        default:
+            return " ";
+        }
     }
 
     /**
@@ -43,6 +96,12 @@ public class Task {
 
     @Override
     public String toString() {
-        return "[" + getStatusIcon() + "] " + getName();
+        String result = "[" + getTypeIcon() + "][" + getStatusIcon() + "] " + getName();
+        if (type == TaskType.EVENT) {
+            result += " (from: " + startDate + " to: " + endDate + ")";
+        } else if (type == TaskType.DEADLINE) {
+            result += " (by: " + endDate + ")";
+        }
+        return result;
     }
 }
